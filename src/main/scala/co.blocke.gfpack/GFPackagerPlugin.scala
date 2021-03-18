cd7a218b405a2febfab9d485e08bb4226ba0a3a9
Greg Zoller <gzoller@gmail.com>
2021-03-18 12:32:43 -0500

    branch detection fix

--- src/main/scala/co.blocke.gfpack/GFPackagerPlugin.scala
+++ src/main/scala/co.blocke.gfpack/GFPackagerPlugin.scala
@@ -28,7 +28,9 @@ object GFPackagerPlugin extends AutoPlugin {
   override lazy val projectSettings = Seq(
     artifactName := { (sv: ScalaVersion, module: ModuleID, artifact: Artifact) =>
       val commit = "git rev-parse --verify HEAD".!! take (COMMIT_SIZE)
-      "git rev-parse --abbrev-ref HEAD".!! trim match {
+      println("!!! Branch: "+ "git branch --show-current".!!)
+      "git branch --show-current".!! trim match {
+      // "git rev-parse --abbrev-ref HEAD".!! trim match {
         case "master" | "main" => {
           val masterVer = (getLatestTag.!!).trim
           artifact.name + "-" + masterVer + artifact.classifier.map(c => s"-$c").getOrElse("") + "." + artifact.extension
