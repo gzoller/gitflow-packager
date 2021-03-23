81f556a4775e04e975ad887913441b50317a09d5
Greg Zoller <gzoller@gmail.com>
2021-03-23 12:52:04 -0500

    show env

--- src/main/scala/co.blocke.gfpack/GFPackagerPlugin.scala
+++ src/main/scala/co.blocke.gfpack/GFPackagerPlugin.scala
@@ -29,6 +29,10 @@ object GFPackagerPlugin extends AutoPlugin {
     artifactName := { (sv: ScalaVersion, module: ModuleID, artifact: Artifact) =>
       val commit = "git rev-parse --verify HEAD".!! take (COMMIT_SIZE)
       "git rev-parse --abbrev-ref HEAD".!! trim match {
+        case "HEAD" => 
+          val env = System.getenv()
+          println("ENV: "+env)
+          artifac.name + "_bogus"
         case "master" | "main" => {
           val masterVer = (getLatestTag.!!).trim
           artifact.name + "-" + masterVer + artifact.classifier.map(c => s"-$c").getOrElse("") + "." + artifact.extension
